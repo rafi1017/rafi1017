e875dcfc2a5a13f832fb68e7862a2976036cddc9
Rafi1017 <56899042+rafi1017@users.noreply.github.com>
2020-05-27 20:38:19 +0200

    Update main.asm

--- NASM/X86/WIN/When Heuristics Go Bad/main.asm
+++ NASM/X86/WIN/When Heuristics Go Bad/main.asm
@@ -1,18 +1,9 @@
-; helloworld.asm
-;
-; This is a Win32 console program that writes "Hello World"
-; on a single line and then exits.
-;
-; To assemble to .obj: nasm -f win32 helloworld.asm
-; To compile to .exe:  gcc helloworld.obj -o helloworld.exe
-; ------------------------------------------------------------------
-
-        global    _main                ; declare main() method
-        extern    _printf              ; link to external library
+        global    _main                
+        extern    _printf             
 
         segment  .data
-        message: db   'Hello world', 0xA, 0  ; text message
-        message2: db   'Hello2', 0xA, 0  ; text message
+        message: db   'Hello world', 0xA, 0  
+        message2: db   'Hello2', 0xA, 0 
 
         ; code is put in the .text section
         section .text
@@ -29,9 +20,9 @@ _func:
 		push eax
 		ret 	
 		
-_main:                            ; the entry point! void main()
-        push    message           ; save message to the stack
-		call 	_func 			  ; Funkció!
-        call    _printf           ; display the first value on the stack
-        add     esp, 4            ; clear the stack
-        ret                       ; return
+_main:                           
+        push    message          
+		call 	_func 			  
+        call    _printf           
+        add     esp, 4           
+        ret                     
